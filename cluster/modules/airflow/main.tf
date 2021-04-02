cf6ff9fcaa4ddbdd5da43f0a758ebca386239f0e
Matheus Jericó Palhares <matheusjerico1994@hotmail.com>
2021-04-02 20:27:31 -0300

    adding integration with github

--- cluster/modules/airflow/main.tf
+++ cluster/modules/airflow/main.tf
@@ -1,4 +1,4 @@
-resource "helm_release" "airflow-stable" {
+resource "helm_release" "airflow" {
   name       = "airflow"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "airflow"
@@ -8,4 +8,40 @@ resource "helm_release" "airflow-stable" {
     name  = "executor"
     value = "CeleryExecutor"
   }
+
+  set {
+    name  = "auth.username"
+    value = "admin"
+  }
+
+  set {
+    name  = "auth.password"
+    value = "admin"
+  }
+
+  set {
+    name  = "git.dags.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "git.dags.repositories[0].repository"
+    value = "https://matheusjerico:<PERSONAL_TOKEN_GIT>@github.com/matheusjerico/spark-on-kubernetes.git"
+  }
+
+  set {
+    name  = "git.dags.repositories[0].name"
+    value = "github"
+  }
+
+  set {
+    name  = "git.dags.repositories[0].branch"
+    value = "master"
+  }
+
+  set {
+    name  = "git.dags.repositories[0].path"
+    value = "dags"
+  }
+
 }
